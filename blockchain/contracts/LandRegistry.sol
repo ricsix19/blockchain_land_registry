9c71012d03a9d8fc9e804b75e34dc6ca39b52d4f
ricsix19 <richardladanyi30@gmail.com>
2026-03-16 15:56:33 +0100

    feat: add Property struct and mapping for data storage

--- blockchain/contracts/LandRegistry.sol
+++ blockchain/contracts/LandRegistry.sol
@@ -1,10 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-
 contract LandRegistry {
+    // State variable to store the address of the contract administrator
     address public Admin;
 
     constructor() {
         Admin = msg.sender;
     }
+
+    // Struct to represent a land parcel
+    struct Property{
+        uint id;
+        string location;
+        uint price;
+        address owner;
+        bool isForSale;
+    }
+
+    mapping(uint => Property) public properties;
+    uint public propertyCount = 0;
 }
