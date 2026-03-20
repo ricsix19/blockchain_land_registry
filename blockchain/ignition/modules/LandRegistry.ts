06d6ef2cf84e070825b1bebac21d8b6afce50ea4
ladanrch <ladanrch@schaeffler.com>
2026-03-20 11:52:15 +0100

    feat: add Hardhat Ignition deployment module for LandRegistry

---
+++ blockchain/ignition/modules/LandRegistry.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const LandRegistryModule = buildModule("LandRegistryModule", (m) => {
+
+    const landRegistry = m.contract("LandRegistry");
+
+    return { landRegistry };
+});
+
+export default LandRegistryModule;
